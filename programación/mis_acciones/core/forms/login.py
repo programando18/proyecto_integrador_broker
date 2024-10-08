a42992291bb8ab4ac22dc6488f123b104476ac94
Christian Caracach <christiancaracach@gmail.com>
2024-10-08 18:21:42 -0300

    6: creacion de formulario login

---
+++ programación/mis_acciones/core/forms/login.py
@@ -0,0 +1,6 @@
+def formulario_login():
+    print("===== Iniciar sesión =====")
+    email = input("Ingrese su correo electrónico: ")
+    contraseña = input("Ingrese su contraseña: ")
+    print("==========================")
+    return [email, contraseña]
