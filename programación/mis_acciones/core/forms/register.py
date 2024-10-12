154b5e970b253ef49d7f5c5e235bcc16844fbd3a
Milagros Cabrera <maguicabrera23@gmail.com>
2024-10-12 09:25:58 -0300

    Modificamos Register

--- programación/mis_acciones/core/forms/register.py
+++ programación/mis_acciones/core/forms/register.py
@@ -4,14 +4,15 @@ def ingresar_email(self):
         print("---------- Ingreso de Correo Electrónico ----------")
         while True: 
             email_input = input("Ingrese su email: ")
+
             if self.validar_email(email_input):
                 print(f"Correo electrónico ingresado correctamente, su email es: {email_input}.")
-                self.email = email_input  # Guardamos el email ingresado
+                self.__email = email_input  # Guardamos el email ingresado
                 break
             else:
                 print("Correo electrónico no válido. Intente nuevamente.")
         print("---------------------------------------------------")
-        return self.email
+        return self.__email
 
 def validar_email(self, email):
         """Encapsula la validación del formato del correo electrónico."""
@@ -21,11 +22,12 @@ def validar_email(self, email):
 
 def obtener_email(self):
         """Permite acceder al email almacenado de forma controlada."""
-        if self.email:
-            return self.email
+        if self.__email:
+            return self.__email
         else:
             return "No se ha ingresado un correo electrónico."
 
 
-form_ingreso_email()    
+ingresar_email()    
 
+# Falta llamar a la clase y sus metodos con su objeto. 
