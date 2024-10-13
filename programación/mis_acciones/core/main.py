20b7f79aa1ba44a765542d039aa0c46720225f76
Christian Caracach <christiancaracach@gmail.com>
2024-10-12 22:40:23 -0300

    cree la parte básica del panel de control

--- programación/mis_acciones/core/main.py
+++ programación/mis_acciones/core/main.py
@@ -8,6 +8,8 @@ class Usuario:
         self.nombre = nombre
         self.total_invertido = total_invertido
         self.rendimiento = rendimiento
+        self.saldo = "1000"
+        self.acciones = ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]
 
 
 def main():
@@ -19,8 +21,22 @@ def main():
         return
     while opcion != "4":
         if opcion == "1":
-            print(formulario_login())
-            # print(panel_de_control(usuario))
+            datos_login = formulario_login()
+
+            # Validar datos_login
+            # Si están validados entonces:
+            opcion = panel_de_control(usuario)
+
+            while opcion != "4":
+                if opcion == "1":
+                    print("Comprar")
+                elif opcion == "2":
+                    print("Vender")
+                elif opcion == "3":
+                    print("Ver gráfico")
+                else:
+                    print("Opción inválida")
+                opcion = panel_de_control(usuario)
             # Acá va la lógica de login
         elif opcion == "2":
             # Acá va el formulario de recuperar contraseña
