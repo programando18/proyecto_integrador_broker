46a6026af02dec8b5648af1e9960e21953646d86
Milagros Cabrera <maguicabrera23@gmail.com>
2024-10-16 23:38:32 -0300

    Modificamos las tablas y sus claves foraneas

--- BBDD/arg_broker.sql
+++ BBDD/arg_broker.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE arg_Broker;
+CREATE DATABASE arg_broker;
 
-USE arg_Broker;
+USE arg_broker;
 
 CREATE TABLE inversor (
     id_inversor INT PRIMARY KEY AUTO_INCREMENT,
@@ -12,10 +12,10 @@ CREATE TABLE inversor (
     id_contacto INT,
     id_tipo_inversor INT,
     fecha_alta DATE,
-    FOREIGN KEY (id_contacto) REFERENCES Tipo_contacto(id_contacto),
-    FOREIGN KEY (id_tipo_inversor) REFERENCES Tipo_inversor(id_tipo_inversor)
+    pregunta VARCHAR(100), 
+    respuesta VARCHAR(100),
+    id_portafolio INT
 );
-
 
 CREATE TABLE tipo_contacto (
     id_contacto INT PRIMARY KEY AUTO_INCREMENT,
@@ -38,33 +38,47 @@ CREATE TABLE acción (
 
 CREATE TABLE historico_cotizaciones (
     id_historico INT PRIMARY KEY AUTO_INCREMENT,
-    id_accion INT,
     fecha_cotizacion DATE NOT NULL,
     precio_compra FLOAT NOT NULL,
-    precio_venta FLOAT NOT NULL,
-    FOREIGN KEY (id_accion) REFERENCES Accion(id_accion)
+    precio_venta FLOAT NOT NULL
 );
 
 CREATE TABLE portafolio (
     id_portafolio INT PRIMARY KEY AUTO_INCREMENT,
     id_inversor INT,
-    id_accion INT,
-    cantidad_acciones INT NOT NULL,
     rendimiento FLOAT,
     total_invertido FLOAT,
-    saldo FLOAT,
-    FOREIGN KEY (id_inversor) REFERENCES Inversor(id_inversor),
-    FOREIGN KEY (id_accion) REFERENCES Accion(id_accion)
+    saldo FLOAT
 );
 
 CREATE TABLE transaccion (
     id_transaccion INT PRIMARY KEY AUTO_INCREMENT,
     id_inversor INT,
-    id_accion INT,
     tipo_operacion VARCHAR(6) NOT NULL,  -- "compra" o "venta"
     fecha DATE NOT NULL,
     monto FLOAT NOT NULL,
-    comisión FLOAT NOT NULL,
-    FOREIGN KEY (id_inversor) REFERENCES Inversor(id_inversor),
-    FOREIGN KEY (id_accion) REFERENCES Accion(id_accion)
+    comisión FLOAT NOT NULL
 );
+
+
+--CREAR LAS FK, QUE CONCUERDEN CON EL MODELO RELACIONAL 
+ALTER TABLE inversor
+ADD CONSTRAINT fk_id_contacto FOREIGN KEY (id_contacto) REFERENCES tipo_contacto(id_contacto);
+
+
+ALTER TABLE inversor
+ADD CONSTRAINT fk_id_tipo_inversor FOREIGN KEY (id_tipo_inversor) REFERENCES tipo_inversor(id_tipo_inversor);
+
+
+ALTER TABLE portafolio
+ADD CONSTRAINT fk_id_inversor FOREIGN KEY (id_inversor) REFERENCES inversor(id_inversor);
+
+
+ALTER TABLE transaccion
+ADD CONSTRAINT fk_id_inversor_transaccion FOREIGN KEY (id_inversor) REFERENCES inversor(id_inversor);
+
+
+
+
+
+
